8d44074f6d838addddd6b8b4d7798e4eb398170c
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-14 01:13:58 -0500

    Update deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,14 +15,24 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving(katz_deli)
-    if katz_deli.empty?
-       puts "There is nobody waiting to be served!"
-    else status = ["Currently serving:"]
-        katz_deli.each.with_index(1) do |name|
-            status.push("#{name}")
-        end
-        puts status.join(" ") 
-    end
-end
+# def now_serving(katz_deli)
+#     if katz_deli.empty?
+#        puts "There is nobody waiting to be served!"
+#     else status = ["Currently serving:"]
+#         katz_deli.each.with_index(1) do |name|
+#             status.push("#{name}")
+#         end
+#         puts status.join(" ") 
+#     end
+# end
 
+def now_serving(katz_deli)
+    if katz_deli.empty? == true
+       puts "There is nobody waiting to be served!"
+    else
+       puts "Currently serving #{katz_deli[0]}." 
+       katz_deli.shift
+
+    end
+
+end
